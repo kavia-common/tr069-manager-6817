df6cc2ca9449ba24ea0da952d65f06e2fcd610b1
Edward NAUWELAERTS <edward.nauwelaerts_ext@softathome.com>
2022-06-09 13:26:00 +0200

    Issue: HOP-379 [Gitlab CI][Unit tests][valgrind] Pipeline doesn't stop when memory leaks are detected [other]
    GitOrigin-RevId: 659ef639f8217121536d59590310d6417c2217de

--- test/test_targets.mk
+++ test/test_targets.mk
@@ -1,8 +1,8 @@
 all: $(TARGET)
 
 run: $(TARGET)
-	set -o pipefail; valgrind --error-exitcode=1 ./$< 2>&1 | tee -a $(OBJDIR)/unit_test_results.txt;
-
+	set -o pipefail; valgrind --leak-check=full --exit-on-first-error=yes --error-exitcode=1 ./$< 2>&1 | tee -a $(OBJDIR)/unit_test_results.txt;
+	
 $(TARGET): $(OBJECTS)
 	$(CC) -o $@ $(OBJECTS) $(LDFLAGS) -fprofile-arcs -ftest-coverage
 
