8f9ac458fe6b62a627e9042fbf67865d3b07e665
sahbot <support.opensource@softathome.com>
2023-10-24 10:22:55 +0200

    Issue: HOP-3453 [Coverage] Remove SAHTRACE defines in order to increase branching coverage
    GitOrigin-RevId: b0fc03471dde062b57b0c5ba8e4f8f6115aed6de

--- test/test_defines.mk
+++ test/test_defines.mk
@@ -11,8 +11,7 @@ CFLAGS += -Wall -Wextra -Wno-attributes --std=gnu99 -g3 \
 		  $(addprefix -I ,$(INCDIR)) -I$(OBJDIR)/.. \
 		  -fkeep-inline-functions -fkeep-static-functions \
 		  -Wno-format-nonliteral \
-		  $(shell pkg-config --cflags cmocka) -pthread -DUNIT_TEST \
-		  -DSAHTRACES_ENABLED -DSAHTRACES_LEVEL=500
+		  $(shell pkg-config --cflags cmocka) -pthread -DUNIT_TEST
 
 LDFLAGS += -fkeep-inline-functions -fkeep-static-functions \
 		   $(shell pkg-config --libs cmocka) \
