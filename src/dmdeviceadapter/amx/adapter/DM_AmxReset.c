4bf00d38acaedfd2fb643427d2dd6b91bae8487b
sahbot <support.opensource@softathome.com>
2024-03-19 18:28:57 +0100

    Issue: HOP-6153 [Reboot Reason][tr-069] Provide the correct Reboot reason
    GitOrigin-RevId: 05ae2c4f6355f0dacf54fd1858eec04447296c65

--- src/dmdeviceadapter/amx/adapter/DM_AmxReset.c
+++ src/dmdeviceadapter/amx/adapter/DM_AmxReset.c
@@ -83,10 +83,19 @@
 void DM_ENG_Device_Reboot_DoReboot(dm_amx_env_t* amx) {
     SAH_TRACEZ_IN("DM_DA");
     int rv = 0;
-    rv = amxb_call(amx->bus_ctx, OBJNAME, REBOOT, NULL, NULL, 5);
+    amxc_var_t args;
+    amxc_var_init(&args);
+    amxc_var_set_type(&args, AMXC_VAR_ID_HTABLE);
+    amxc_var_add_key(cstring_t, &args, "Cause", "RemoteReboot");
+    amxc_var_add_key(cstring_t, &args, "Reason", "TR-069");
+
+    rv = amxb_call(amx->bus_ctx, OBJNAME, REBOOT, &args, NULL, 5);
     if(rv != 0) {
         SAH_TRACEZ_ERROR("DM_DA", "Invoke failed Device.Reboot - retval = %d", rv);
     }
+
+    amxc_var_clean(&args);
+
     SAH_TRACEZ_OUT("DM_DA");
 }
 //---------------------------------------------------------------------------------------------
@@ -102,10 +111,16 @@ void DM_ENG_Device_Reboot_DoReboot(dm_amx_env_t* amx) {
 void DM_ENG_Device_FactoryReset_DoReset(dm_amx_env_t* amx) {
     SAH_TRACEZ_IN("DM_DA");
     int rv = 0;
-    rv = amxb_call(amx->bus_ctx, OBJNAME, FACTORY_RESET, NULL, NULL, 5);
+    amxc_var_t args;
+    amxc_var_init(&args);
+    amxc_var_set_type(&args, AMXC_VAR_ID_HTABLE);
+    amxc_var_add_key(cstring_t, &args, "Cause", "RemoteFactoryReset");
+    amxc_var_add_key(cstring_t, &args, "Reason", "TR-069");
+    rv = amxb_call(amx->bus_ctx, OBJNAME, FACTORY_RESET, &args, NULL, 5);
     if(rv != 0) {
         SAH_TRACEZ_ERROR("DM_DA", "Invoke failed Device.FactoryReset - retval = %d", rv);
     }
+    amxc_var_clean(&args);
     SAH_TRACEZ_OUT("DM_DA");
 }
 
